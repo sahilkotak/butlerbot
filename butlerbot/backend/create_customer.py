4afd78ceb8b9b7042501926caef7932c495c3c1d
Ramesh Aryal <ramesh.aryal@socoro.com.au>
2023-09-28 22:49:26 +1000

    part of the oAuth configuration and Authorisation is done

--- butlerbot/backend/create_customer.py
+++ butlerbot/backend/create_customer.py
@@ -1,27 +1,57 @@
-# create_customer.py
 import requests
 import json
+import webbrowser
 
-def create_square_customer(access_token, customer_data):
-    create_customer_url = 'https://connect.squareup.com/v2/customers'
+def get_authorization_code(client_id, redirect_uri, scope):
+    authorization_url = f"https://connect.squareup.com/oauth2/authorize?client_id={client_id}&scope={scope}&redirect_uri={redirect_uri}&response_type=code"
+    
+    # Open the authorization URL in a web browser
+    webbrowser.open(authorization_url)
 
-    headers = {
-        'Authorization': 'Bearer EAAAEIQBQf7_4FQj3x63wAB_3ZkLFCRMuSNLthpcMADFzgKzHBPxl2dZNMCVzCdZ',
-        'Content-Type': 'application/json',
-        'Square-Version': '2023-08-16'
+    # Wait for the user to complete authorization and receive the authorization code manually
+    authorization_code = input("Enter the authorization code from the redirected URL: ")
+
+    return authorization_code
+
+def create_square_customer(authorization_code):
+    token_url = "https://connect.squareup.com/oauth2/token"
+
+    token_data = {
+        "client_id": "sq0idp-6aYBSQ_b6TCjM0iJ2viLFw",
+        "client_secret": "sq0csp-CusXW-fZIE25jo29D-GjUNa6dwPOZ02NiIjLKltrjbQ",
+        "code": authorization_code,
+        # "redirect_uri": "http://localhost:8000/test",
+        "redirect_uri": "https://ntibnportal.powerappsportals.com/",
+        "grant_type": "authorization_code",
     }
 
     try:
-        print( "Customer creation started")
-        print(customer_data)
-        print(access_token)
+        print("stated")
+        response = requests.post(token_url, data=token_data)
+        response.raise_for_status()
+        access_token = response.json()["access_token"]
+        print(f"Access Token: {access_token}")
+
+        # Create a customer using the obtained access token
+        create_customer_url = 'https://connect.squareup.com/v2/customers'
+
+        headers = {
+            'Authorization': f'Bearer {access_token}',
+            'Content-Type': 'application/json',
+            'Square-Version': '2023-08-16'
+        }
+
+        customer_data = {
+            "given_name": "Ramesh",
+            "family_name": "Aryal",
+            "email_address": "ramesh.aryal@socoro.com.au",
+        }
+
         response = requests.post(create_customer_url, headers=headers, data=json.dumps(customer_data))
         response.raise_for_status()
 
         customer_info = response.json()
         customer_id = customer_info['customer']['id']
-        return customer_info
-
-    except requests.exceptions.RequestException as e:
-        print(f"Error creating customer: {e}")
-        return None
+        return customer_id  # You may return other relevant information if needed
+    except Exception as e:
+        raise Exception(f"Error creating customer: {e}")
--- butlerbot/backend/main.py
+++ butlerbot/backend/main.py
@@ -2,53 +2,36 @@ import base64
 import json
 import time
 import logging
-
 from fastapi import FastAPI, UploadFile, BackgroundTasks, Header
-from fastapi.responses import FileResponse
-from fastapi.responses import RedirectResponse
+from fastapi.responses import FileResponse, RedirectResponse
 from fastapi.staticfiles import StaticFiles
 
-
-from ai import get_completion
-from stt import transcribe
-from tts import to_speech
-
-# Import the FastAPI app from my_api.py
 from my_api import app as my_api_app
-from create_customer import create_square_customer
 
 app = FastAPI()
 logging.basicConfig(level=logging.INFO)
 
-# Mount the my_api app under a subpath, e.g., /myapi
+# Mount the my_api app under a subpath, e.g., /test
 app.mount("/test", my_api_app)
 
+from create_customer import get_authorization_code, create_square_customer  # Import these functions
+
 @app.post("/create-customer")
-def create_customer():
-    customer_data = {
-    "given_name": "ramesh",
-    "family_name": "aryal",
-    "email_address": "ramesharyal@socoro.com.au",
-    "company_name": "socoro pty ltd",
-    "idempotency_key": "fe5a8692-3f14-4f45-be15-c8e51f54687a",
-    "nickname": "None",
-    "note": "No note",
-    "birthday": "2002-10-02"
-  }
-
+async def authorize_and_create_customer():
     try:
-        # Create a customer using the function
-        customer_id = create_square_customer("EAAAEIQBQf7_4FQj3x63wAB_3ZkLFCRMuSNLthpcMADFzgKzHBPxl2dZNMCVzCdZ", customer_data)
-        print (customer_id, "cssadfasl;d;fl")
-        if customer_id:
-            return {"message": f"Customer created with ID: {customer_id}"}
-        else:
-            return {"error": "Failed to create customer."}
 
+        client_id = "sq0idp-6aYBSQ_b6TCjM0iJ2viLFw"
+        redirect_uri = "https://ntibnportal.powerappsportals.com/"
+
+        authorization_code = get_authorization_code(client_id, redirect_uri, "CUSTOMERS_WRITE")
+        print("authorization_code", authorization_code)
+
+        customer_id = create_square_customer(authorization_code)
+        print(f"Access Token: {customer_id}")
+        
+        return {"message": "Customer created successfully.", "customer_id": customer_id}  # Optionally, return a response to the client
     except Exception as e:
-        print(f"Error creating customer: {e}")
-        return {"error": e}
-
+        return {"error": str(e)}  # Handle exceptions and return an error response if needed
 
 @app.post("/inference")
 async def infer(audio: UploadFile, background_tasks: BackgroundTasks,
@@ -61,20 +44,23 @@ async def infer(audio: UploadFile, background_tasks: BackgroundTasks,
     ai_response_audio_filepath = await to_speech(ai_response_text, background_tasks)
 
     logging.info('total processing time: %s %s', time.time() - start_time, 'seconds')
-    return FileResponse(path=ai_response_audio_filepath, media_type="audio/mpeg",
-                        headers={"text": _construct_response_header(user_prompt_text, ai_response_text)})
-
+    
+    return FileResponse(
+        path=ai_response_audio_filepath,
+        media_type="audio/mpeg",
+        headers={"text": _construct_response_header(user_prompt_text, ai_response_text)}
+    )
 
 @app.get("/")
 async def root():
     return RedirectResponse(url="/index.html")
 
-
-app.mount("/", StaticFiles(directory="../frontend/dist"), name="static")
-
-
 def _construct_response_header(user_prompt, ai_response):
     return base64.b64encode(
         json.dumps(
-            [{"role": "user", "content": user_prompt}, {"role": "assistant", "content": ai_response}]).encode(
-            'utf-8')).decode("utf-8")
+            [{"role": "user", "content": user_prompt}, {"role": "assistant", "content": ai_response}]
+        ).encode('utf-8')
+    ).decode("utf-8")
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="localhost", port=8000)
