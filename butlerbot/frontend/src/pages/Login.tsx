ee1b594da991a2e5d056cf43c8cd827305f515e1
Kevin Patel <kevinmanojpatel@gmail.com>
2023-10-06 19:10:18 +1100

    minor frontend bug fix

--- butlerbot/frontend/src/pages/Login.tsx
+++ butlerbot/frontend/src/pages/Login.tsx
@@ -6,10 +6,13 @@ const Login = () => {
       <Button
         onClick={() => {
           // eslint-disable-next-line no-undef
-          if (!process.env.BUTLERBOT_API_ENDPOINT)
+          if (!process.env.BUTLERBOT_API_ENDPOINT) {
             alert(
               "Application not configured properly. Missing required configurations - BUTLERBOT_API_ENDPOINT."
             );
+
+            return;
+          }
 
           // eslint-disable-next-line no-undef
           window.location.href = `${process.env.BUTLERBOT_API_ENDPOINT}/authorise`;
