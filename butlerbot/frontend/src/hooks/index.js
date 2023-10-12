eb4fca380b60f98ae7ab6ba28be03db5ebac9610
Kevin Patel <kevinmanojpatel@gmail.com>
2023-10-12 22:47:08 +1100

    client side wav encoding

--- butlerbot/frontend/src/hooks/index.js
+++ butlerbot/frontend/src/hooks/index.js
@@ -1,1 +1,2 @@
 export { useCookie } from "./useCookie";
+export { useAudioRecorder } from "./useAudioRecorder";
--- butlerbot/frontend/src/pages/HomePage.tsx
+++ butlerbot/frontend/src/pages/HomePage.tsx
@@ -1,72 +1,42 @@
-import { useState, useRef } from "react";
-import { Link } from "react-router-dom";
-import { Button } from "react-bootstrap";
+import { useState } from "react";
+import { Button, Center } from "@chakra-ui/react";
 
-import { onSpeechStart, onSpeechEnd } from "../utils/speech-manager";
+import { RecorderError } from "../components/";
+import { useAudioRecorder } from "../hooks";
 
 const HomePage = () => {
-  const [audioReady, setAudioReady] = useState(false);
-  const [audioBlob, setAudioBlob] = useState(null);
+  const { startRecording, stopRecording } = useAudioRecorder();
+  const [recorderError, setRecorderError] = useState(false);
 
-  const mediaRecorder = useRef(null);
-  const audioChunks = useRef([]);
-
-  const startRecording = async () => {
-    const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
-
-    mediaRecorder.current = new MediaRecorder(stream);
-
-    mediaRecorder.current.ondataavailable = (event) => {
-      audioChunks.current.push(event.data);
-    };
-
-    mediaRecorder.current.start();
-    onSpeechStart();
-  };
-
-  const stopRecording = async () => {
-    if (mediaRecorder.current) {
-      mediaRecorder.current.onstop = async () => {
-        const recordedBlob = new Blob(audioChunks.current, {
-          type: "audio/wav",
-        });
-
-        setAudioBlob(recordedBlob);
-        setAudioReady(true);
-
-        const processedBlob = await onSpeechEnd(recordedBlob);
-        audioChunks.current = [processedBlob]; // Update audioChunks to contain the processed audio
-      };
-
-      mediaRecorder.current.stop();
-    }
-  };
   return (
-    <>
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          height: "100vh",
-          width: "100vw",
-        }}
-      >
-        <div>
-          <Link to="/login">Login</Link>
-          <Link to="/setup/AADK124243432">Setup Cookie</Link>
-        </div>
-
-        <div>
-          <Button onClick={startRecording}>Start</Button>
-          <Button onClick={stopRecording}>End</Button>
-
-          {audioReady && (
-            <audio controls src={URL.createObjectURL(audioBlob)} />
-          )}
-        </div>
-      </div>
-    </>
+    <Center minHeight="100vh">
+      {recorderError ? (
+        <RecorderError />
+      ) : (
+        <>
+          <Button
+            colorScheme="teal"
+            variant="solid"
+            onClick={async () => {
+              try {
+                await startRecording();
+              } catch (e) {
+                setRecorderError(e);
+              }
+            }}
+          >
+            Start
+          </Button>
+          <Button
+            colorScheme="teal"
+            variant="outline"
+            onClick={async () => await stopRecording()}
+          >
+            Stop
+          </Button>
+        </>
+      )}
+    </Center>
   );
 };
 
