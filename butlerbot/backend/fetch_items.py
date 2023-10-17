55f6206f25a48940769af62a9bea436b563170cc
sahilkotak <40324523+sahilkotak@users.noreply.github.com>
2023-10-18 01:43:40 +1100

    - additional tweaks in AI logic

--- butlerbot/backend/fetch_items.py
+++ butlerbot/backend/fetch_items.py
@@ -7,18 +7,11 @@ import os
 
 class Config:
     DB_REGION_NAME = os.getenv("DB_REGION_NAME", "ap-southeast-2")
-    #DB_ACCESS_KEY_ID = os.getenv("DB_ACCESS_KEY_ID")
-    #DB_SECRET_ACCESS_KEY = os.getenv("DB_SECRET_ACCESS_KEY")
 
-def fetch_items(merchant_id=None, include_description=False):
-    # if merchant_id is None:
-    #     merchant_id = 'MLF15C4CDSG4S'
+def fetch_items(merchant_id=None):
 
     dynamodb = boto3.resource(
         "dynamodb",
-        #region_name=Config.DB_REGION_NAME,
-        #aws_access_key_id=Config.DB_ACCESS_KEY_ID,
-        #aws_secret_access_key=Config.DB_SECRET_ACCESS_KEY
     )
     table = dynamodb.Table('merchandise')
 
@@ -32,21 +25,54 @@ def fetch_items(merchant_id=None, include_description=False):
     # Extract the required details from each item
     items_details = []
     for item in items:
-        item_details = {
-            'item_name': item['item_name'].title(),  # Convert item name to title case
-            'price': str(Decimal(item['price'])),  # Convert Decimal to string to avoid JSON serialization error
-            'variation_name': item['variation_name'],
-            'currency': response['Items'][0]['currency'],
-        }
-        if include_description:
-                item_details['item_description'] = item['item_description']
-        items_details.append(item_details)
+        # Check if the necessary fields are present in the item
+        if 'item_name' in item and 'price' in item and 'variation_name' in item:
+            item_details = {
+                'item_name': item['item_name'].title(),  # Convert item name to title case
+                'price': str(Decimal(item['price'])),  # Convert Decimal to string to avoid JSON serialization error
+                'variation_name': item['variation_name'],
+            }
+            items_details.append(item_details)
         
     # Currency code is stored in the 'currency' field in the DynamoDB table
-    currency_code = response['Items'][0]['currency']
+    currency_code = response['Items'][0]['currency'] if 'currency' in response['Items'][0] else None
 
     # Format the items and currency code into a json
-    # The json will be in the format: {"items": [{"item_description": "", "item_name": "", "price": "", "variation_name": ""}, ...], "currency": ""}
-    items_json = json.dumps({"items": items_details, "currency": currency_code, "message": "This our menu, only allow users to order what's available here."})
+    # The json will be in the format: {"items": [{ "item_name": "", "price": "", "variation_name": ""}, ...], "currency": ""}
+    items_json = json.dumps({"items": items_details, "currency": currency_code})
 
     return items_json
+
+def fetch_item_description(item_name, merchant_id=None):
+
+    dynamodb = boto3.resource(
+        "dynamodb",
+    )
+    table = dynamodb.Table('merchandise')
+
+    # Fetch items for a specific merchant
+    response = table.query(
+        KeyConditionExpression=Key('merchant_id').eq(merchant_id)
+    )
+
+    items = response['Items']
+
+    # Filter items based on item_name
+    items = [item for item in items if item['item_name'] == item_name]
+
+    # Check if items exist
+    if not items:
+        return json.dumps({"items": []})
+
+    # Extract the required details from the first item
+    item = items[0]
+    item_details = {
+        'item_name': item['item_name'].title(),  # Convert item name to title case
+        'item_description': item['item_description'],
+    }
+
+    # Format the item into a json
+    # The json will be in the format: {"item": {"item_description": "", "item_name": ""}}
+    item_json = json.dumps({"item": item_details})
+
+    return item_json
